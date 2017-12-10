b27df1bb88b4ebb23cc47a564e697a60bb2ce234
Robb Giordano <robbgior@gmail.com>
2017-12-10 07:24:32 -0500

    Works on device with remote GCE

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.JavaJokes;
 import com.example.android.androidjokes.JokeActivity;
+import com.example.rgiordano.myapplication.backendjokes.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Robbert"));
     }
 
 
@@ -51,6 +61,46 @@ public class MainActivity extends AppCompatActivity {
         //Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
 
         launchJokeActivity(view);
+    }
+}
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("https://robbjokes.appspot.com/_ah/api/");
+                    //.setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                    //    @Override
+                    //    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                    //        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    //    }
+                    //});
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
     }
 }
 
---
+++ backendjokes/src/main/java/com/example/rgiordano/myapplication/backendjokes/MyBean.java
@@ -0,0 +1,17 @@
+package com.example.rgiordano.myapplication.backendjokes;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
